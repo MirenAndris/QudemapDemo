769dfd22dbe94a75db1d2287113f093ad69510e9
MirenAndris <98027035+MirenAndris@users.noreply.github.com>
2022-01-26 14:48:51 +0200

    Add files via upload

---
+++ modules/dataScraper.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+
+#pievienot beautifulsoup4 pie requirements.txt GitHubā
+
+def apstrada_lapu(url):
+    r = requests.get(url)
+    html = r.text
+    soup = BeautifulSoup(html, 'html.parser')
+    return soup
+
+def scrapeData(url, filters):
+    data = []
+    imgURLdata = []	
+    html = apstrada_lapu("https://www.ss.lv/lv/real-estate/flats/rezekne-and-reg/")
+
+    data = html.find_all('tr', class_='class="msga2 pp0"')
+    for row in data:
+        imageURL = re.findall('<img.*?src="(.*?)"[^>]+>/g',row)
+        imgURLdata.append(imageURL)
+    return imgURLdata
+
