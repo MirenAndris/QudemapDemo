3828548456613bc596aaff339b7ed9b731c6900a
MirenAndris <andris.geduss@atv.rta.lv>
2022-01-26 23:58:36 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 from flask import Flask
-from flask import request
+from flask import requestpip
 from flask import url_for
 from flask import render_template, make_response
+from modules import dataScraper
 import os
 
 app = Flask(__name__)
