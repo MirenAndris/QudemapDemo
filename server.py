6ec8105dcb25681f3d0f2134bc4e33811c18b56e
MirenAndris <98027035+MirenAndris@users.noreply.github.com>
2022-01-27 13:14:38 +0200

    Update server.py
    labojums

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def root():
     pictures = dataScraper.scrapeImg("","")
-    return render_template("index.html"),pictures=pictures)
+    return render_template("index.html",pictures=pictures)
 
 @app.route('/karte')
 def raditKarti():
