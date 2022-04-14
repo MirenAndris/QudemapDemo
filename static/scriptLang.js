411c56108a9914c6320f29b0f5d0516963c678ed
MirenAndris <98027035+MirenAndris@users.noreply.github.com>
2022-04-14 12:09:08 +0300

    Binding the translation function

---
+++ static/scriptLang.js
@@ -0,0 +1,8 @@
+function setLanguageLV() {
+    document.cookie = "language=latvian";
+    window.location.reload(true);
+}
+function setLanguageEN() {
+    document.cookie = "language=english";
+    window.location.reload(true);
+}
