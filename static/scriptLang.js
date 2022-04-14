313cbbf6a5bcfe6d5a7ab08ec81832e1284d453d
MirenAndris <98027035+MirenAndris@users.noreply.github.com>
2022-04-14 14:01:05 +0300

    all script reload

--- static/scriptLang.js
+++ static/scriptLang.js
@@ -1,8 +1,8 @@
 function setLanguageLV() {
-    document.cookie = "language=LAT";
+    document.cookie = "language=latvian";
     window.location.reload(true);
 }
 function setLanguageEN() {
-    document.cookie = "language=ENG";
+    document.cookie = "language=english";
     window.location.reload(true);
 }
